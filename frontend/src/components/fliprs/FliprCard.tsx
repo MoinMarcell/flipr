38b3e4dbaecc0ffa376afb3b5caa263e80aaedbf
MoinMarcell <marcell.dechant@protonmail.com>
2022-12-16 18:34:04 +0100

    Create card for fliprs

---
+++ frontend/src/components/fliprs/FliprCard.tsx
@@ -0,0 +1,42 @@
+import * as React from 'react';
+import {Flipr} from "../models/Flipr";
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import Avatar from '@mui/material/Avatar';
+import Typography from '@mui/material/Typography';
+import Divider from "@mui/material/Divider";
+import avatar from "./img/avatars/dolphin.png"
+
+type FliprCardProps = {
+    flipr: Flipr
+}
+
+export default function FliprCard(props: FliprCardProps){
+    return (
+        <span>
+            <ListItem alignItems="flex-start">
+                <ListItemAvatar>
+                    <Avatar alt="Remy Sharp" src={avatar} />
+                </ListItemAvatar>
+                <ListItemText
+                    primary={props.flipr.content}
+                    secondary={
+                        <React.Fragment>
+                            <Typography
+                                sx={{ display: 'inline' }}
+                                component="span"
+                                variant="body2"
+                                color="text.primary"
+                            >
+                                by {props.flipr.author.username}
+                            </Typography>
+                            {" — postet 2h ago"}
+                        </React.Fragment>
+                    }
+                />
+            </ListItem>
+            <Divider variant="inset" component="li" />
+        </span>
+    )
+}
