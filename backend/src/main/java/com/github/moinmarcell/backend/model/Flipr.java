2615d7d4a9198c3d7277994b5729ccd55e058d5d
MoinMarcell <marcell.dechant@protonmail.com>
2022-12-16 14:40:25 +0100

    Create flipr model

---
+++ backend/src/main/java/com/github/moinmarcell/backend/model/Flipr.java
@@ -0,0 +1,13 @@
+package com.github.moinmarcell.backend.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document("fliprs")
+public record Flipr(
+        @Id
+        String id,
+        String content,
+        Author author
+){
+}
