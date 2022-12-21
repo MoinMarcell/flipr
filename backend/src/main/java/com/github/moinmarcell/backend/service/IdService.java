824237abafec9dc89388cede14db307239a7848b
Marcell <84642104+MoinMarcell@users.noreply.github.com>
2022-12-21 15:31:18 +0100

    Update visibility for id service

--- backend/src/main/java/com/github/moinmarcell/backend/service/IdService.java
+++ backend/src/main/java/com/github/moinmarcell/backend/service/IdService.java
@@ -5,8 +5,8 @@ import org.springframework.stereotype.Service;
 import java.util.UUID;
 
 @Service
-public class IdService {
-    public String generateId(){
+class IdService {
+    String generateId(){
         return UUID.randomUUID().toString();
     }
 }
