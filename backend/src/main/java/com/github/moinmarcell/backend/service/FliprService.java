027d6e1bba66ad55731b4fd899f3458c15ae997c
Marcell <84642104+MoinMarcell@users.noreply.github.com>
2022-12-21 13:16:41 +0100

    Update service & add method to save a flipr

--- backend/src/main/java/com/github/moinmarcell/backend/service/FliprService.java
+++ backend/src/main/java/com/github/moinmarcell/backend/service/FliprService.java
@@ -1,6 +1,7 @@
 package com.github.moinmarcell.backend.service;
 
 import com.github.moinmarcell.backend.model.Flipr;
+import com.github.moinmarcell.backend.model.FliprDTO;
 import com.github.moinmarcell.backend.repo.FliprRepository;
 import org.springframework.stereotype.Service;
 
@@ -10,12 +11,24 @@ import java.util.List;
 public class FliprService {
 
     FliprRepository fliprRepository;
+    IdService idService;
 
-    public FliprService(FliprRepository fliprRepository) {
+    public FliprService(FliprRepository fliprRepository, IdService idService) {
         this.fliprRepository = fliprRepository;
+        this.idService = idService;
     }
 
-    public List<Flipr> getAllFliprs(){
+    public List<Flipr> getAllFliprs() {
         return fliprRepository.findAll();
+    }
+
+    public Flipr saveFlipr(FliprDTO fliprDTO) {
+        Flipr fliprToSave = new Flipr(
+                idService.generateId(),
+                fliprDTO.content(),
+                fliprDTO.author()
+        );
+        fliprRepository.save(fliprToSave);
+        return fliprToSave;
     }
 }
