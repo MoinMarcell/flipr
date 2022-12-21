e7f48f34c7b89f95555e9952e986bd03417464b7
Marcell <84642104+MoinMarcell@users.noreply.github.com>
2022-12-21 13:12:28 +0100

    Update dto for flipr

--- backend/src/main/java/com/github/moinmarcell/backend/model/FliprDTO.java
+++ backend/src/main/java/com/github/moinmarcell/backend/model/FliprDTO.java
@@ -1,8 +1,5 @@
 package com.github.moinmarcell.backend.model;
 
-import org.springframework.data.mongodb.core.mapping.Document;
-
-@Document("fliprs")
 public record FliprDTO(
         String content,
         Author author
