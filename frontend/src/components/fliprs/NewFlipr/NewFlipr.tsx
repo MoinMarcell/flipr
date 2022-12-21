83beede1992a7f79c7da0b35e1ef1ed7e2e39339
Marcell <84642104+MoinMarcell@users.noreply.github.com>
2022-12-21 14:26:02 +0100

    Update new flipr

--- frontend/src/components/fliprs/NewFlipr/NewFlipr.tsx
+++ frontend/src/components/fliprs/NewFlipr/NewFlipr.tsx
@@ -1,18 +1,40 @@
 import {Button, TextField} from "@mui/material";
+import {ChangeEvent, FormEvent, useState} from "react";
 
-const NewFlipr = () => {
+type NewFliprProps = {
+    content: (content:string) => void
+}
+
+const NewFlipr = (props: NewFliprProps) => {
+
+    const [content, setContent] = useState<string>("")
+
+    function handleContentChange(event: ChangeEvent<HTMLInputElement>) {
+        const changedInput = event.target.value;
+        setContent(changedInput);
+    }
+
+    function handleSubmit(event: FormEvent<HTMLFormElement>){
+        event.preventDefault();
+        props.content(content);
+        setContent("");
+    }
+
     return (
         <div>
-            <TextField
-                id="outlined-multiline-static"
-                label="Your flipr"
-                multiline
-                rows={4}
-                fullWidth
-            />
-            <Button variant="contained" sx={{mt: 1}}>Contained</Button>
+            <form onSubmit={handleSubmit}>
+                <TextField
+                    id="outlined-multiline-static"
+                    label="Your flipr"
+                    multiline
+                    rows={4}
+                    fullWidth
+                    onChange={handleContentChange}
+                />
+                <Button type={"submit"} variant="contained" sx={{mt: 1}}>Contained</Button>
+            </form>
         </div>
-    )
+    );
 }
 
 export default NewFlipr;
