e4af9c1a52cd8932cf2c310924902f4ffce90c53
Marcell <84642104+MoinMarcell@users.noreply.github.com>
2022-12-22 08:41:16 +0100

    Update new flipr component & extracted form to own component

--- frontend/src/components/fliprs/NewFlipr/NewFlipr.tsx
+++ frontend/src/components/fliprs/NewFlipr/NewFlipr.tsx
@@ -1,38 +1,16 @@
-import {Button, TextField} from "@mui/material";
-import {ChangeEvent, FormEvent, useState} from "react";
+import NewFliprForm from "./NewFliprForm";
 
 type NewFliprProps = {
-    content: (content:string) => void
+    onSaveFlipr: (content:string) => void
 }
 
 const NewFlipr = (props: NewFliprProps) => {
-
-    const [content, setContent] = useState<string>("")
-
-    function handleContentChange(event: ChangeEvent<HTMLInputElement>) {
-        const changedInput = event.target.value;
-        setContent(changedInput);
+    const saveFliprHandler = (content: string) => {
+        props.onSaveFlipr(content);
     }
-
-    function handleSubmit(event: FormEvent<HTMLFormElement>){
-        event.preventDefault();
-        props.content(content);
-        setContent("");
-    }
-
     return (
         <div>
-            <form onSubmit={handleSubmit}>
-                <TextField
-                    id="outlined-multiline-static"
-                    label="Your flipr"
-                    multiline
-                    rows={4}
-                    fullWidth
-                    onChange={handleContentChange}
-                />
-                <Button type={"submit"} variant="contained" sx={{mt: 1}}>flipr it now!</Button>
-            </form>
+            <NewFliprForm onSaveFlipr={saveFliprHandler} />
         </div>
     );
 }
