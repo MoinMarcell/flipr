98e49b6759541079e92ed11feb3cd6631303ac0d
Marcell <84642104+MoinMarcell@users.noreply.github.com>
2023-01-03 15:02:32 +0100

    Update user controller

--- backend/src/main/java/com/github/moinmarcell/backend/security/UserController.java
+++ backend/src/main/java/com/github/moinmarcell/backend/security/UserController.java
@@ -1,9 +1,12 @@
 package com.github.moinmarcell.backend.security;
 
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpSession;
 import java.security.Principal;
 
 @RestController
@@ -12,6 +15,21 @@ public class UserController {
 
     @GetMapping("/me")
     public String helloMe(Principal principal){
-        return "Hallo " + principal.getName();
+        if(principal != null){
+            return principal.getName();
+        }
+        return "anonymousUser";
+    }
+
+    @PostMapping("/login")
+    public String login(){
+        return SecurityContextHolder.getContext().getAuthentication().getName();
+    }
+
+    @PostMapping("/logout")
+    public String logout(HttpSession httpSession){
+        httpSession.invalidate();
+        SecurityContextHolder.clearContext();
+        return "anonymousUser";
     }
 }
