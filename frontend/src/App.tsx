80797ae685d93951c9f7c58f6b3a7b969dd6662a
Marcell <84642104+MoinMarcell@users.noreply.github.com>
2023-01-03 14:59:13 +0100

    Update app with user hooks

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,19 +3,25 @@ import Navigation from "./components/Navigation/Navigation";
 import FliprsApp from "./components/Fliprs/FliprsApp";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Login from "./components/Login/Login";
+import useUser from "./components/Hooks/useUser";
 
 const App = () => {
+
+    const {username, login, logout} = useUser();
+
     return (
         <BrowserRouter>
             <div className={"container-xxl"}>
                 <div className={"row"}>
                     <div className={"col text-center border-end border-light mt-2"}>
-                        <Navigation/>
+                        <Navigation logout={logout}/>
+                        <h2 className={"text-white"}>Hello, {username}</h2>
                     </div>
                     <div className={"col-10 mt-2"}>
+                        <h2 className={"text-white text-center"}><i className="fa-solid fa-terminal"></i> FLIPR</h2>
                         <Routes>
                             <Route path={"/"} element={<FliprsApp />} />
-                            <Route path={"/login"} element={<Login />} />
+                            <Route path={"/login"} element={<Login login={login} />} />
                         </Routes>
                     </div>
                 </div>
