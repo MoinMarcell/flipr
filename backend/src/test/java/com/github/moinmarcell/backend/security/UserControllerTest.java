2bce8dbf9c406f74a41dc0298729a42dacd3afa5
Marcell <84642104+MoinMarcell@users.noreply.github.com>
2023-01-03 15:03:00 +0100

    Create test for user controller

---
+++ backend/src/test/java/com/github/moinmarcell/backend/security/UserControllerTest.java
@@ -0,0 +1,42 @@
+package com.github.moinmarcell.backend.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class UserControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    @WithMockUser
+    void helloMe_expectUser() throws Exception {
+        mockMvc.perform(get("/api/users/me"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("user"));
+    }
+
+    @Test
+    void helloMe_expectAnonymousUser() throws Exception {
+        mockMvc.perform(get("/api/users/me"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("anonymousUser"));
+    }
+
+    @Test
+    @WithMockUser(username = "marcell")
+    void helloMe_myUser() throws Exception {
+        mockMvc.perform(get("/api/users/me"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("marcell"));
+    }
+}
