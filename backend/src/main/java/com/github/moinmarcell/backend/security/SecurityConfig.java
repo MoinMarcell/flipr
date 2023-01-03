f94dbcd63cc7794ccdb161ddf1975d116dfafb33
Marcell <84642104+MoinMarcell@users.noreply.github.com>
2023-01-03 09:01:55 +0100

    Create SecurityConfig

---
+++ backend/src/main/java/com/github/moinmarcell/backend/security/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.github.moinmarcell.backend.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .csrf().disable()
+                .httpBasic().and()
+                .authorizeRequests()
+                .antMatchers("/api/fliprs").authenticated()
+                .antMatchers("/").permitAll()
+                .and().build();
+    }
+
+}
