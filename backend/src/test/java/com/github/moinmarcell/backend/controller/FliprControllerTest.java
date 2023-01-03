a1d5ce8c4dfa09079641e93bf6d829be56c8f483
Marcell <84642104+MoinMarcell@users.noreply.github.com>
2023-01-03 14:59:52 +0100

    Update FliprControllerTest & add annotations & add csrf

--- backend/src/test/java/com/github/moinmarcell/backend/controller/FliprControllerTest.java
+++ backend/src/test/java/com/github/moinmarcell/backend/controller/FliprControllerTest.java
@@ -8,11 +8,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -33,16 +35,19 @@ class FliprControllerTest {
 
     @Test
     @DirtiesContext
+    @WithMockUser
     void onApiCall_returnEmptyListJsonFormat() throws Exception{
-        mockMvc.perform(get("/api/fliprs"))
+        mockMvc.perform(get("/api/fliprs").with(csrf()))
                 .andExpect(status().isOk())
                 .andExpect(content().json("[]"));
     }
 
     @Test
     @DirtiesContext
+    @WithMockUser
     void whenApiCalledToSaveOneFlipr_thenReturnSavedFlipr() throws Exception {
         MvcResult result = mockMvc.perform(post("/api/fliprs")
+                        .with(csrf())
                         .contentType(MediaType.APPLICATION_JSON)
                         .content("{}"))
                 .andExpect(status().isOk())
