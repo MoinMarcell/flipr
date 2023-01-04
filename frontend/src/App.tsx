6e84854333b8dbeccab90a259d7c6930d3c34ae5
Marcell <84642104+MoinMarcell@users.noreply.github.com>
2023-01-04 11:48:31 +0100

    Update app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,26 +2,25 @@ import "./app.css";
 import Navigation from "./components/Navigation/Navigation";
 import FliprsApp from "./components/Fliprs/FliprsApp";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
-import Login from "./components/Login/Login";
+import LoginRegisterPage from "./components/LoginRegisterPage/LoginRegisterPage";
 import useUser from "./components/Hooks/useUser";
 
 const App = () => {
 
-    const {username, login, logout} = useUser();
+    const {username, login, logout, register} = useUser();
 
     return (
         <BrowserRouter>
             <div className={"container-xxl"}>
                 <div className={"row"}>
                     <div className={"col text-center border-end border-light mt-2"}>
-                        <Navigation logout={logout}/>
-                        <h2 className={"text-white"}>Hello, {username}</h2>
+                        <Navigation logout={logout} username={username}/>
                     </div>
                     <div className={"col-10 mt-2"}>
                         <h2 className={"text-white text-center"}><i className="fa-solid fa-terminal"></i> FLIPR</h2>
                         <Routes>
                             <Route path={"/"} element={<FliprsApp />} />
-                            <Route path={"/login"} element={<Login login={login} />} />
+                            <Route path={"/login"} element={<LoginRegisterPage login={login} register={register} />} />
                         </Routes>
                     </div>
                 </div>
