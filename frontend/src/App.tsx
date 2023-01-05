854d67941e25b8e2ae99b767f41b3d20bb2ca43a
MoinMarcell <marcell.dechant@protonmail.com>
2023-01-05 16:49:08 +0100

    Update App

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,8 @@ import FliprsApp from "./components/Fliprs/FliprsApp";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import LoginRegisterPage from "./components/LoginRegisterPage/LoginRegisterPage";
 import useUser from "./components/Hooks/useUser";
+import ProtectedRoutes from "./components/ProtectedRoutes";
+import MyProfile from "./components/Profile/MyProfile";
 
 const App = () => {
 
@@ -13,14 +15,18 @@ const App = () => {
         <BrowserRouter>
             <div className={"container-xxl"}>
                 <div className={"row"}>
-                    <div className={"col text-center border-end border-light mt-2"}>
+                    <div className={"col-4 text-end border-end border-light mt-2"}>
                         <Navigation logout={logout} username={username}/>
                     </div>
-                    <div className={"col-10 mt-2"}>
+                    <div className={"col-8 mt-2"}>
                         <h2 className={"text-white text-center"}><i className="fa-solid fa-terminal"></i> FLIPR</h2>
                         <Routes>
                             <Route path={"/"} element={<FliprsApp />} />
                             <Route path={"/login"} element={<LoginRegisterPage login={login} register={register} />} />
+
+                            <Route element={<ProtectedRoutes username={username} />}>
+                                <Route path={"/my-profile"} element={<MyProfile username={username} />} />
+                            </Route>
                         </Routes>
                     </div>
                 </div>
