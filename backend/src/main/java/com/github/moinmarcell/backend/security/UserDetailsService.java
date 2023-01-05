1e067aa452d11a1ec6a4293337f0e73a592b6f4e
MoinMarcell <marcell.dechant@protonmail.com>
2023-01-05 16:48:27 +0100

    Update UserDetailsService

--- backend/src/main/java/com/github/moinmarcell/backend/security/UserDetailsService.java
+++ backend/src/main/java/com/github/moinmarcell/backend/security/UserDetailsService.java
@@ -2,23 +2,22 @@ package com.github.moinmarcell.backend.security;
 
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
-public class MongoUserDetailsService implements UserDetailsService {
-    private final MongoUserRepo mongoUserRepo;
+public class UserDetailsService implements org.springframework.security.core.userdetails.UserDetailsService {
+    private final UserRepo userRepo;
 
-    public MongoUserDetailsService(MongoUserRepo mongoUserRepo) {
-        this.mongoUserRepo = mongoUserRepo;
+    public UserDetailsService(UserRepo userRepo) {
+        this.userRepo = userRepo;
     }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return mongoUserRepo.findByUsername(username)
+        return userRepo.findByUsername(username)
                 .map(foundUser -> new User(foundUser.username(), foundUser.password(), List.of()))
                 .orElseThrow(() -> new UsernameNotFoundException(username + " not found!"));
     }
