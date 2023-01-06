82525fabc8b8e3ecc3abdbd17b19d6be21c21e68
MoinMarcell <marcell.dechant@protonmail.com>
2023-01-06 14:09:45 +0100

    Add unique index to username and mail & update id to fliprID

--- backend/src/main/java/com/github/moinmarcell/backend/model/FliprUser.java
+++ backend/src/main/java/com/github/moinmarcell/backend/model/FliprUser.java
@@ -1,16 +1,19 @@
 package com.github.moinmarcell.backend.model;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.List;
 
 @Document("users")
 public record FliprUser(
+        @Id
         String fliprID,
+        @Indexed(unique = true)
         String username,
         String password,
-        @Id
+        @Indexed(unique = true)
         String email,
         List<Flipr> fliprList
 ){
