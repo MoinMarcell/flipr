ddd408a8f116466a832f0b9a1e27645074955f10
MoinMarcell <marcell.dechant@protonmail.com>
2023-01-09 19:53:57 +0100

    Create hooks for flipr & user

---
+++ frontend/src/components/Hooks/useUser.tsx
@@ -0,0 +1,37 @@
+import {useEffect, useState} from "react";
+import axios from "axios";
+
+export default function useUser(){
+    const [username, setUsername] = useState<string>("");
+
+    useEffect(() => {
+        axios.get("/api/users/me")
+            .then(r => r.data)
+            .then(setUsername);
+    }, [])
+
+    function login(username: string, password: string) {
+        return axios.post("/api/users/login", undefined, {
+            auth: {
+                "username": username,
+                "password": password
+            }
+        })
+            .then((r) => r.data)
+            .then(data => {
+                setUsername(data)
+                return data
+            })
+    }
+
+    function logout(){
+        return axios.post("/api/users/logout")
+            .then((r) => r.data)
+            .then((data) => {
+                setUsername(data)
+                return data
+            })
+    }
+
+    return {username, login, logout}
+}
