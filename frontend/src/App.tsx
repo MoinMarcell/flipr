a9b407d8c81e289e6d771f72d6493bc728f5e774
MoinMarcell <marcell.dechant@protonmail.com>
2023-01-09 19:52:20 +0100

    Update app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,49 +1,32 @@
 import NavigationBar from "./components/AppBar/NavigationBar";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
-import axios from "axios";
-import {useEffect, useState} from "react";
 import Container from "@mui/material/Container";
-import Register from "./components/Register";
+import Register from "./components/Register/Register";
+import FliprApp from "./components/Flipr/FliprApp";
+import AddFlipr from "./components/Flipr/AddFlipr";
+import Profile from "./components/Profile/Profile";
+import useUser from "./components/Hooks/useUser";
+import useFliprs from "./components/Hooks/useFliprs";
+import ProtectedRoutes from "./components/ProtectedRoutes";
 
 const App = () => {
 
-    const [username, setUsername] = useState<string>("");
-
-    useEffect(() => {
-        axios.get("/api/users/me")
-            .then(r => r.data)
-            .then(setUsername)
-    }, [])
-
-    function login(username: string, password: string) {
-        return axios.post("/api/users/login", undefined, {
-            auth: {
-                "username": username,
-                "password": password
-            }
-        })
-            .then((r) => r.data)
-            .then(data => {
-                setUsername(data)
-                return data
-            });
-    }
-
-    function logout(){
-        return axios.post("/api/users/logout")
-            .then((r) => r.data)
-            .then((data) => {
-                setUsername(data)
-                return data
-            })
-    }
+    const {username, login, logout} = useUser();
+    const {fliprs, saveFlipr, deleteFlipr} = useFliprs();
 
     return (
         <BrowserRouter>
             <NavigationBar handleLogout={logout} handleLogin={login} username={username}/>
             <Container sx={{mt: 10}}>
+                <AddFlipr handleSubmit={saveFlipr} username={username}/>
                 <Routes>
-                    <Route path={"/register"} element={<Register />} />
+                    <Route path={"/"}
+                           element={<FliprApp fliprs={fliprs} username={username} handleDelete={deleteFlipr}/>}/>
+                    <Route path={"/register"} element={<Register/>}/>
+                    <Route element={<ProtectedRoutes username={username} />}>
+                        <Route path={"/profile"}
+                               element={<Profile username={username} fliprs={fliprs} handleDelete={deleteFlipr}/>}/>
+                    </Route>
                 </Routes>
             </Container>
         </BrowserRouter>
