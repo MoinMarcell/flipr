e0c94f05b1014a0db1a8c995c240527f424ad383
MoinMarcell <marcell.dechant@protonmail.com>
2023-01-09 13:54:25 +0100

    Remade Login

--- backend/src/main/java/com/github/moinmarcell/backend/service/FliprUserService.java
+++ backend/src/main/java/com/github/moinmarcell/backend/service/FliprUserService.java
@@ -1,8 +1,6 @@
 package com.github.moinmarcell.backend.service;
 
-import com.github.moinmarcell.backend.model.FliprUser;
-import com.github.moinmarcell.backend.model.FliprUserDTO;
-import com.github.moinmarcell.backend.model.FliprUserRegistrationDTO;
+import com.github.moinmarcell.backend.model.*;
 import com.github.moinmarcell.backend.repo.FliprUserRepo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.crossstore.ChangeSetPersister;
@@ -20,37 +18,35 @@ public class FliprUserService {
     private final IdService idService;
     private final Argon2Service argon2Service;
 
-    public FliprUserDTO saveFliprUser(FliprUserRegistrationDTO fliprUserRegistrationDTO){
+    public FliprUserDTO saveFliprUser(FliprUserRegistrationDTO fliprUserRegistrationDTO) {
         FliprUser fliprUserToSave = new FliprUser(
                 idService.generateId(),
                 fliprUserRegistrationDTO.username(),
                 argon2Service.encode(fliprUserRegistrationDTO.password()),
                 "",
                 new ArrayList<>(),
-                new ArrayList<>());
+                new ArrayList<>()
+        );
+        fliprUserRepo.save(fliprUserToSave);
 
-        FliprUserDTO user = new FliprUserDTO(
+        return new FliprUserDTO(
                 fliprUserToSave.id(),
                 fliprUserToSave.username(),
                 fliprUserToSave.avatar(),
                 fliprUserToSave.fliprs(),
                 fliprUserToSave.favFliprs());
-
-        fliprUserRepo.save(fliprUserToSave);
-
-        return user;
     }
 
-    public List<FliprUserDTO> getAllFliprUsers(){
+    public List<FliprUserDTO> getAllFliprUsers() {
         List<FliprUser> allFliprUsers = fliprUserRepo.findAll();
         return allFliprUsers.stream().map(fliprUser ->
-            new FliprUserDTO(fliprUser.id(), fliprUser.username(), fliprUser.avatar(), fliprUser.fliprs(), fliprUser.favFliprs()))
+                        new FliprUserDTO(fliprUser.id(), fliprUser.username(), fliprUser.avatar(), fliprUser.fliprs(), fliprUser.favFliprs()))
                 .toList();
     }
 
     public FliprUserDTO getFliprUserByUsername(String username) throws ChangeSetPersister.NotFoundException {
         Optional<FliprUser> fliprUserByUsername = fliprUserRepo.findByUsername(username);
-        if(fliprUserByUsername.isEmpty()){
+        if (fliprUserByUsername.isEmpty()) {
             throw new ChangeSetPersister.NotFoundException();
         }
         return new FliprUserDTO(fliprUserByUsername.get().id(), fliprUserByUsername.get().username(), fliprUserByUsername.get().avatar(), fliprUserByUsername.get().fliprs(), fliprUserByUsername.get().favFliprs());
