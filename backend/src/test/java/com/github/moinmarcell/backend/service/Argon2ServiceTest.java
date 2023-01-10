047426966af6f1be89868110048f80705d9d585e
Marcell <marcell.dechant@proton.me>
2023-01-10 15:54:25 +0100

    Create test for Argon2Service

---
+++ backend/src/test/java/com/github/moinmarcell/backend/service/Argon2ServiceTest.java
@@ -0,0 +1,16 @@
+package com.github.moinmarcell.backend.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Argon2ServiceTest {
+
+    Argon2Service argon2Service = new Argon2Service();
+
+    @Test
+    void encode_WhenPasswordEncodedExpectNotNull() {
+        String password =  "123";
+        assertNotNull(argon2Service.encode(password));
+    }
+}
