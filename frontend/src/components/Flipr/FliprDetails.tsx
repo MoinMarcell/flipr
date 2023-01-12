95819859de901a21a3325f1dc18e08061d2df4f0
MoinMarcell <marcell.dechant@protonmail.com>
2023-01-12 16:20:03 +0100

    Update flipr details

--- frontend/src/components/Flipr/FliprDetails.tsx
+++ frontend/src/components/Flipr/FliprDetails.tsx
@@ -1,10 +1,12 @@
 import FliprCard from "./FliprCard";
 import {useParams} from "react-router-dom";
 import useFlipr from "../Hooks/useFlipr";
+import {Flipr} from "../Model/Flipr";
 
 type FliprDetailsProps = {
     username: string,
     handleDelete: (id: string | undefined) => void,
+    handleLike: (username: string, flipr: Flipr) => void,
 }
 
 export default function FliprDetails(props: FliprDetailsProps){
@@ -16,6 +18,6 @@ export default function FliprDetails(props: FliprDetailsProps){
     const {flipr} = useFlipr(id)
 
     return(
-        <FliprCard flipr={flipr} username={props.username} handleDelte={props.handleDelete} />
+        <FliprCard handleLike={props.handleLike} flipr={flipr} username={props.username} handleDelte={props.handleDelete} />
     )
 }
