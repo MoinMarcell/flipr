804d29f0cfd5785b6c96017f254a9995a74e4526
MoinMarcell <marcell.dechant@protonmail.com>
2023-01-12 17:00:54 +0100

    Update test

--- backend/src/test/java/com/github/moinmarcell/backend/service/FliprUserServiceTest.java
+++ backend/src/test/java/com/github/moinmarcell/backend/service/FliprUserServiceTest.java
@@ -2,6 +2,7 @@ package com.github.moinmarcell.backend.service;
 
 import com.github.moinmarcell.backend.model.FliprUser;
 import com.github.moinmarcell.backend.model.FliprUserDTO;
+import com.github.moinmarcell.backend.repo.FliprRepository;
 import com.github.moinmarcell.backend.repo.FliprUserRepo;
 import org.junit.jupiter.api.Test;
 
@@ -15,7 +16,8 @@ class FliprUserServiceTest {
     IdService idService = mock(IdService.class);
     Argon2Service argon2Service = mock(Argon2Service.class);
     FliprUserRepo fliprUserRepo = mock(FliprUserRepo.class);
-    FliprUserService fliprUserService = new FliprUserService(fliprUserRepo, idService, argon2Service);
+    FliprRepository fliprRepository = mock(FliprRepository.class);
+    FliprUserService fliprUserService = new FliprUserService(fliprUserRepo, idService, argon2Service, fliprRepository);
 
     @Test
     void saveFliprUser() {
