d7c87fb8526428b13a67fd34cee4eb27b2afbcd8
Marcell <84642104+MoinMarcell@users.noreply.github.com>
2023-01-13 16:10:21 +0100

    Rename props

--- frontend/src/components/Profile/ProfileApp.tsx
+++ frontend/src/components/Profile/ProfileApp.tsx
@@ -40,7 +40,7 @@ function TabPanel(props: TabPanelProps) {
     );
 }
 
-function a11yProps(index: number) {
+function tabProps(index: number) {
     return {
         id: `simple-tab-${index}`,
         'aria-controls': `simple-tabpanel-${index}`,
@@ -59,9 +59,9 @@ export default function ProfileApp(props: ProfileAppProps) {
             <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
                 <h2>Welcome back @{props.username}</h2>
                 <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
-                    <Tab label="My Fliprs" {...a11yProps(0)} />
-                    <Tab label="My liked Fliprs" {...a11yProps(1)} />
-                    <Tab label="Edit my Profile" {...a11yProps(2)} />
+                    <Tab label="My Fliprs" {...tabProps(0)} />
+                    <Tab label="My liked Fliprs" {...tabProps(1)} />
+                    <Tab label="Edit my Profile" {...tabProps(2)} />
                 </Tabs>
             </Box>
             <TabPanel value={value} index={0}>
