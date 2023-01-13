6229e56e61adf28f5b7fc946a5dff5ef6a1a4221
Marcell <84642104+MoinMarcell@users.noreply.github.com>
2023-01-13 14:01:58 +0100

    Update flipr app

--- frontend/src/components/Flipr/FliprApp.tsx
+++ frontend/src/components/Flipr/FliprApp.tsx
@@ -1,17 +1,21 @@
 import FliprGallery from "./FliprGallery";
 import {Flipr} from "../Model/Flipr";
+import AddFlipr from "./AddFlipr";
+import * as React from "react";
 
 type FliprAppProps = {
     fliprs: Flipr[],
     username: string,
     handleDelete: (id: string | undefined) => void,
     searchText: string,
+    saveFlipr: (content: string, username: string) => void,
 }
 
 export default function FliprApp(props: FliprAppProps){
 
     return(
         <main>
+            <AddFlipr handleSubmit={props.saveFlipr} username={props.username} />
             <FliprGallery searchText={props.searchText} fliprs={props.fliprs} username={props.username} handleDelte={props.handleDelete} />
         </main>
     );
