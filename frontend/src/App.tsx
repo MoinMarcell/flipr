0115e76440f801a5a8bb58434e0bf0ca11aef3dc
Marcell <84642104+MoinMarcell@users.noreply.github.com>
2023-01-18 14:11:16 +0100

    Update app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,27 @@
 import * as React from "react";
 import FliprApp from "./components/Flipr/FliprApp";
-import LoginForm from "./components/LoginForm";
 import useUsers from "./components/hooks/useUsers";
 import {Box, Container} from "@mui/material";
-import NavBar from "./components/NavBar";
+import NavBarApp from "./components/NavBar/NavBarApp";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Register from "./components/Register";
+import PublicProfile from "./components/FliprUser/PublicProfile";
+import LoginApp from "./components/login/LoginApp";
 
 const App = () => {
 
-    const {username, login, logout} = useUsers();
+    const {username, login, logout, saveUser, isLoading} = useUsers();
 
     return (
         <Box>
             <BrowserRouter>
-                <NavBar username={username} logout={logout}/>
+                <NavBarApp username={username} logout={logout}/>
                 <Container maxWidth={false} sx={{mt: 2}}>
                     <Routes>
                         <Route path={"/"} element={<FliprApp username={username}/>}/>
-                        <Route path={"/login"} element={<LoginForm username={username} login={login}/>}/>
+                        <Route path={"/login"} element={<LoginApp login={login}/>}/>
+                        <Route path={"/register"} element={<Register register={saveUser} isLoading={isLoading} />} />
+                        <Route path={"/:username"} element={<PublicProfile />} />
                     </Routes>
                 </Container>
             </BrowserRouter>
