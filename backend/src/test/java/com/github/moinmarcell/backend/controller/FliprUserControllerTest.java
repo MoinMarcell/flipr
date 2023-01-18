3d7641e29730b6c627f9e9de39388f9da53a3636
MoinMarcell <marcell.dechant@protonmail.com>
2023-01-18 19:13:27 +0100

    Update FliprUserControllerTest

--- backend/src/test/java/com/github/moinmarcell/backend/controller/FliprUserControllerTest.java
+++ backend/src/test/java/com/github/moinmarcell/backend/controller/FliprUserControllerTest.java
@@ -11,11 +11,9 @@ import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 
 import java.util.Collections;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -25,6 +23,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 class FliprUserControllerTest {
 
+    private final String BASE_URL = "/api/users";
+
     @Autowired
     FliprUserRepo fliprUserRepo;
 
@@ -33,4 +33,70 @@ class FliprUserControllerTest {
 
     @Autowired
     ObjectMapper objectMapper;
+
+    @Test
+    @DirtiesContext
+    @WithMockUser("Flipr")
+    void helloMe_whenLoggedIn_thenReturnUsername() throws Exception {
+        mockMvc.perform(get(BASE_URL + "/me").with(csrf()))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Flipr"));
+    }
+
+    @Test
+    @DirtiesContext
+    void helloMe_whenNotLoggedIn_thenReturnAnonymousUser() throws Exception {
+        mockMvc.perform(get(BASE_URL + "/me").with(csrf()))
+                .andExpect(status().isOk())
+                .andExpect(content().string("anonymousUser"));
+    }
+
+    @Test
+    @DirtiesContext
+    @WithMockUser("Flipr")
+    void login_whenLogin_thenReturnUsername() throws Exception {
+        mockMvc.perform(post(BASE_URL + "/login").with(csrf()))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Flipr"));
+    }
+
+    @Test
+    @DirtiesContext
+    @WithMockUser("Flipr")
+    void logout_whenLogout_thenReturnAnonymousUser() throws Exception {
+        mockMvc.perform(post(BASE_URL + "/logout").with(csrf()))
+                .andExpect(status().isOk())
+                .andExpect(content().string("anonymousUser"));
+    }
+
+    @Test
+    @DirtiesContext
+    void saveFliprUser() throws Exception {
+        mockMvc.perform(post(BASE_URL + "/register")
+                        .with(csrf())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("""
+                                {
+                                    "username": "username",
+                                    "password": "password"
+                                }
+                                """))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @DirtiesContext
+    void getFliprUserByUsername() throws Exception {
+        FliprUser fliprUser = new FliprUser("1", "username", "123", Collections.emptyList());
+        fliprUserRepo.save(fliprUser);
+        mockMvc.perform(get(BASE_URL + "/username").with(csrf()))
+                .andExpect(status().isOk())
+                .andExpect(content().json("""
+                        {
+                            "id": "1",
+                            "username": "username",
+                            "fliprs": []
+                        }
+                        """));
+    }
 }
