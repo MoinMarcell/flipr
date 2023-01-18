af44990d2a2ffe105511a776307cc5dcfb785202
MoinMarcell <marcell.dechant@protonmail.com>
2023-01-18 23:13:54 +0100

    Create test for controller & service

---
+++ backend/src/test/java/com/github/moinmarcell/backend/controller/CommentControllerTest.java
@@ -0,0 +1,45 @@
+package com.github.moinmarcell.backend.controller;
+
+import com.github.moinmarcell.backend.model.Flipr;
+import com.github.moinmarcell.backend.repo.FliprRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.time.LocalDateTime;
+import java.util.Collections;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@AutoConfigureMockMvc
+@SpringBootTest
+class CommentControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    FliprRepository fliprRepository;
+
+    @Test
+    void saveComment() throws Exception {
+        Flipr flipr = new Flipr("1", "content", "author", LocalDateTime.now(), Collections.emptyList());
+        fliprRepository.save(flipr);
+        mockMvc.perform(post("/api/comments")
+                        .with(csrf())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("""
+                                {
+                                    "content": "content",
+                                    "author": "author",
+                                    "fliprId": "1"
+                                }
+                                """))
+                .andExpect(status().isOk());
+    }
+}
---
+++ backend/src/test/java/com/github/moinmarcell/backend/service/CommentServiceTest.java
@@ -0,0 +1,43 @@
+package com.github.moinmarcell.backend.service;
+
+import com.github.moinmarcell.backend.model.Comment;
+import com.github.moinmarcell.backend.model.CommentDTO;
+import com.github.moinmarcell.backend.model.Flipr;
+import com.github.moinmarcell.backend.repo.CommentRepo;
+import com.github.moinmarcell.backend.repo.FliprRepository;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+class CommentServiceTest {
+
+    CommentRepo commentRepo = mock(CommentRepo.class);
+    IdService idService = mock(IdService.class);
+    LocalDateService localDateService = mock(LocalDateService.class);
+    FliprRepository fliprRepository = mock(FliprRepository.class);
+
+    CommentService commentService = new CommentService(commentRepo, idService, localDateService, fliprRepository);
+
+    @Test
+    void saveComment_whenCommentSaved_thenReturnComment() {
+        Comment expected = new Comment("1", "content", "author", LocalDateTime.of(1, 1, 1, 1, 1));
+        CommentDTO commentToSave = new CommentDTO("content", "author", "1");
+        Flipr flipr = new Flipr("1", "content", "author", LocalDateTime.of(1, 1, 1, 1, 1), new ArrayList<>());
+        flipr.comments().add(expected);
+
+        when(commentRepo.save(expected)).thenReturn(expected);
+        when(idService.generateId()).thenReturn("1");
+        when(localDateService.getDate()).thenReturn(LocalDateTime.of(1, 1, 1, 1, 1));
+        when(fliprRepository.findById("1")).thenReturn(Optional.of(flipr));
+
+        Comment actual = commentService.saveComment(commentToSave);
+
+        assertEquals(actual, expected);
+        verify(commentRepo).save(expected);
+    }
+}
