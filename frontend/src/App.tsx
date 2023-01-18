d78e78616daa4562178f04c37d809cd8c06fbdc5
MoinMarcell <marcell.dechant@protonmail.com>
2023-01-18 22:42:05 +0100

    Update app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,6 +6,7 @@ import NavBarApp from "./components/NavBar/NavBarApp";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import PublicProfile from "./components/FliprUser/PublicProfile";
 import LoginRegisterApp from "./components/login/LoginRegisterApp";
+import FliprDetails from "./components/Flipr/FliprDetails";
 
 const App = () => {
 
@@ -18,9 +19,10 @@ const App = () => {
                 <Container maxWidth={false} sx={{mt: 2}}>
                     <Routes>
                         <Route path={"/"} element={<FliprApp username={username}/>}/>
+                        <Route path={"/flipr/:id"} element={<FliprDetails username={username} />} />
                         <Route path={"/register"} element={<LoginRegisterApp register={saveUser} login={login}/>}/>
                         <Route path={"/login"} element={<LoginRegisterApp register={saveUser} login={login}/>}/>
-                        <Route path={"/:username"} element={<PublicProfile />} />
+                        <Route path={"/user/:username"} element={<PublicProfile />} />
                     </Routes>
                 </Container>
             </BrowserRouter>
