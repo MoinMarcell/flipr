0b605d771785adad22f74adf504d06333706f2fd
Marcell <84642104+MoinMarcell@users.noreply.github.com>
2023-01-19 09:34:34 +0100

    Update flipr details

--- frontend/src/components/Flipr/FliprDetails.tsx
+++ frontend/src/components/Flipr/FliprDetails.tsx
@@ -11,13 +11,16 @@ import CardActions from "@mui/material/CardActions";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 import ShareIcon from "@mui/icons-material/Share";
 import CommentIcon from "@mui/icons-material/Comment";
-import {Box} from "@mui/material";
+import {Alert, Box, Divider, Snackbar, Stack, Tooltip} from "@mui/material";
 import Card from "@mui/material/Card";
 import PostComment from "./PostComment";
 import FliprComments from "./FliprComments";
 import Badge from "@mui/material/Badge";
 import {Input} from "@mui/joy";
-import {useCallback} from "react";
+import React, {useCallback} from "react";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import RedditIcon from "@mui/icons-material/Reddit";
 
 type FliprDetailsProps = {
     username: string,
@@ -29,6 +32,19 @@ export default function FliprDetails(props: FliprDetailsProps) {
     const id: string | undefined = params.id;
     const {flipr, postComment} = useFlipr(props.username, id);
     const navigate = useNavigate();
+    const [openSnackBar, setOpenSnackBar] = React.useState(false);
+
+    const handleOpenSnackBar = useCallback(() => {
+        setOpenSnackBar(true);
+    }, [setOpenSnackBar]);
+
+    const handleCloseSnackBar = useCallback((event?: React.SyntheticEvent | Event, reason?: string) => {
+        if (reason === 'clickaway') {
+            return;
+        }
+
+        setOpenSnackBar(false);
+    }, [setOpenSnackBar]);
 
     const date: Date = new Date(flipr.dateTime);
     const months: string[] = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
@@ -38,7 +54,26 @@ export default function FliprDetails(props: FliprDetailsProps) {
     
     const handleLoginClick = useCallback(() => {
         navigate("/login");
-    }, [navigate])
+    }, [navigate]);
+
+    const longText = (
+        <Stack
+            direction="row"
+            divider={<Divider orientation="vertical" sx={{bgcolor: '#fff'}} flexItem />}
+            spacing={2}
+        >
+            <FacebookIcon />
+            <GitHubIcon />
+            <RedditIcon />
+        </Stack>
+    );
+
+    const handleShareClick = useCallback(() => {
+        navigator.clipboard.writeText(window.location.href)
+            .then(() => {
+                handleOpenSnackBar();
+            });
+    }, [handleOpenSnackBar, flipr.id]);
 
     return (
         <Box>
@@ -66,9 +101,17 @@ export default function FliprDetails(props: FliprDetailsProps) {
                     <IconButton aria-label="add to favorites">
                         <FavoriteIcon/>
                     </IconButton>
-                    <IconButton aria-label="share">
-                        <ShareIcon/>
-                    </IconButton>
+                    <Tooltip title={longText} placement="top" arrow>
+                        <IconButton aria-label="share"
+                                    onClick={handleShareClick}>
+                            <ShareIcon/>
+                        </IconButton>
+                    </Tooltip>
+                    <Snackbar open={openSnackBar} autoHideDuration={6000} onClose={handleCloseSnackBar}>
+                        <Alert onClose={handleCloseSnackBar} severity="success" sx={{width: '100%'}}>
+                            Link copied!
+                        </Alert>
+                    </Snackbar>
                     <IconButton aria-label={"comment"}>
                         <Badge badgeContent={flipr.comments.length} color="primary">
                             <CommentIcon/>
