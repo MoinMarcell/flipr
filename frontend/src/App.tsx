dc5ad06cd4eb87e0556b3721b8ab0bd2f3e01318
Marcell <84642104+MoinMarcell@users.noreply.github.com>
2023-01-19 09:47:34 +0100

    Update app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import FliprApp from "./components/Flipr/FliprApp";
 import useUsers from "./components/hooks/useUsers";
-import {Box, Container} from "@mui/material";
+import {Container} from "@mui/material";
 import NavBarApp from "./components/NavBar/NavBarApp";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import PublicProfile from "./components/FliprUser/PublicProfile";
@@ -13,20 +13,18 @@ const App = () => {
     const {username, login, logout, saveUser} = useUsers();
 
     return (
-        <Box>
-            <BrowserRouter>
-                <NavBarApp username={username} logout={logout}/>
-                <Container maxWidth={false} sx={{mt: 2}}>
-                    <Routes>
-                        <Route path={"/"} element={<FliprApp username={username}/>}/>
-                        <Route path={"/flipr/:id"} element={<FliprDetails username={username} />} />
-                        <Route path={"/register"} element={<LoginRegisterApp register={saveUser} login={login}/>}/>
-                        <Route path={"/login"} element={<LoginRegisterApp register={saveUser} login={login}/>}/>
-                        <Route path={"/user/:username"} element={<PublicProfile username={username} />} />
-                    </Routes>
-                </Container>
-            </BrowserRouter>
-        </Box>
+        <BrowserRouter>
+            <NavBarApp username={username} logout={logout}/>
+            <Container maxWidth={false} sx={{mt: 2}}>
+                <Routes>
+                    <Route path={"/"} element={<FliprApp username={username}/>}/>
+                    <Route path={"/flipr/:id"} element={<FliprDetails username={username}/>}/>
+                    <Route path={"/register"} element={<LoginRegisterApp register={saveUser} login={login}/>}/>
+                    <Route path={"/login"} element={<LoginRegisterApp register={saveUser} login={login}/>}/>
+                    <Route path={"/user/:username"} element={<PublicProfile username={username}/>}/>
+                </Routes>
+            </Container>
+        </BrowserRouter>
     );
 
 }
