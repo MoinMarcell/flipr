c937105b0e8d45aaad4191fb94596795c2fd5b56
Marcell <84642104+MoinMarcell@users.noreply.github.com>
2023-01-23 15:51:40 +0100

    Update app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,17 +5,21 @@ import FliprApp from "./components/flipr/FliprApp";
 import useFliprs from "./components/hooks/useFliprs";
 import useUsers from "./components/hooks/useUsers";
 import LoginApp from "./components/login/LoginApp";
+import Navigation from "./components/navigation/Navigation";
+import FliprDetails from "./components/flipr/FliprDetails";
 
 export default function App(){
 
-    const {fliprs, saveFlipr, deleteFlipr} = useFliprs();
-    const {username, login} = useUsers();
+    const {fliprs, saveFlipr, deleteFlipr, likeFlipr} = useFliprs();
+    const {username, login, logout} = useUsers();
 
     return (
         <BrowserRouter>
-            <Container maxWidth={false} sx={{mt: 10}}>
+            <Navigation logout={logout} username={username} />
+            <Container maxWidth={false} sx={{mt: 3}}>
                 <Routes>
-                    <Route path={"/"} element={<FliprApp deleteFlipr={deleteFlipr} saveFlipr={saveFlipr} username={username} fliprs={fliprs} />} />
+                    <Route path={"/"} element={<FliprApp likeFlipr={likeFlipr} deleteFlipr={deleteFlipr} saveFlipr={saveFlipr} username={username} fliprs={fliprs} />} />
+                    <Route path={"/flipr/:id"} element={<FliprDetails likeFlipr={likeFlipr} deleteFlipr={deleteFlipr} username={username} />} />
                     <Route path={"/login"} element={<LoginApp login={login} />} />
                 </Routes>
             </Container>
