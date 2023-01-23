b1746ffef23c1287dd463973cf6d38fb70245405
Marcell <84642104+MoinMarcell@users.noreply.github.com>
2023-01-23 15:53:12 +0100

    Update FliprApp

--- frontend/src/components/flipr/FliprApp.tsx
+++ frontend/src/components/flipr/FliprApp.tsx
@@ -2,13 +2,14 @@ import {Flipr} from "../model/Flipr";
 import {Box} from "@mui/material";
 import FliprGallery from "./FliprGallery";
 import FliprPost from "./FliprPost";
-import {FliprDTO} from "../model/FliprDTO";
+import {CommentDTO} from "../model/FliprDTO";
 
 type FliprAppProps = {
     fliprs: Flipr[],
     username: string,
-    saveFlipr(fliprToSave: FliprDTO): Promise<Flipr>,
+    saveFlipr(fliprToSave: CommentDTO): Promise<Flipr>,
     deleteFlipr(fliprId: string): Promise<string>,
+    likeFlipr(fliprId: string): Promise<string>,
 }
 
 export default function FliprApp(props: FliprAppProps){
@@ -16,7 +17,7 @@ export default function FliprApp(props: FliprAppProps){
     return(
         <Box>
             <FliprPost saveFlipr={props.saveFlipr} author={props.username} />
-            <FliprGallery deleteFlipr={props.deleteFlipr} username={props.username} fliprs={props.fliprs} />
+            <FliprGallery likeFlipr={props.likeFlipr} deleteFlipr={props.deleteFlipr} username={props.username} fliprs={props.fliprs} />
         </Box>
     );
 
