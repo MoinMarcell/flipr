8cab90403b7f09125600ce2127f06ee13f8c38d3
MoinMarcell <marcell.dechant@protonmail.com>
2023-01-27 00:11:37 +0100

    Update app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,10 @@
 import * as React from "react";
-import {BrowserRouter, Routes} from "react-router-dom";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 import FliprTopBar from "./components/appBar/FliprTopBar";
 import useUsers from "./components/hooks/useUsers";
+import UserProfile from "./components/profile/UserProfile";
+import PublicProfile from "./components/profile/PublicProfile";
+import ProtectedRoutes from "./ProtectedRoutes";
 
 export default function App(){
 
@@ -11,7 +14,10 @@ export default function App(){
         <BrowserRouter>
             <FliprTopBar logout={logout} login={login} username={username} isAuthenticated={isAuthenticated} />
             <Routes>
-
+                <Route path={"/:username"} element={<PublicProfile />} />
+                <Route element={<ProtectedRoutes login={login} username={username} />}>
+                    <Route path={"/my-profile"} element={<UserProfile username={username} />} />
+                </Route>
             </Routes>
         </BrowserRouter>
     );
