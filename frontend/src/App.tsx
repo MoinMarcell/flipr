3bd2fc3facb4dd4737bbdf6419f0551415737d26
MoinMarcell <marcell.dechant@protonmail.com>
2023-01-26 18:20:48 +0100

    Update app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,19 @@
 import * as React from "react";
+import {BrowserRouter, Routes} from "react-router-dom";
+import FliprTopBar from "./components/appBar/FliprTopBar";
+import useUsers from "./components/hooks/useUsers";
 
 export default function App(){
 
+    const {username, isAuthenticated, login, logout} = useUsers();
+
     return (
-        <h1>Welcome to FLIPR!</h1>
+        <BrowserRouter>
+            <FliprTopBar logout={logout} login={login} username={username} isAuthenticated={isAuthenticated} />
+            <Routes>
+
+            </Routes>
+        </BrowserRouter>
     );
 
 }
