a20a36d64b53069ad28e67cec30ffc0d000a8ab3
MoinMarcell <marcell.dechant@protonmail.com>
2023-01-27 00:13:55 +0100

    Update ProtectedRoutes

--- frontend/src/ProtectedRoutes.tsx
+++ frontend/src/ProtectedRoutes.tsx
@@ -1,7 +1,9 @@
 import {Navigate, Outlet} from "react-router-dom";
+import {FliprUserDTO} from "./components/models/FliprUserDTO";
 
 type ProtectedRoutesProps = {
     username: string,
+    login: (userToLogin: FliprUserDTO) => Promise<unknown>,
 }
 
 const ProtectedRoutes = (props: ProtectedRoutesProps) => {
@@ -9,7 +11,11 @@ const ProtectedRoutes = (props: ProtectedRoutesProps) => {
     const isAuthenticated = props.username !== 'anonymousUser' && props.username !== undefined && props.username !== null;
 
     return (
-        isAuthenticated ? <Outlet /> : <Navigate to={"/"} />
+        isAuthenticated
+            ?
+            <Outlet/>
+            :
+            <Navigate to={"/"} />
     );
 }
 
