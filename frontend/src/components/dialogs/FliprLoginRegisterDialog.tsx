5d28075d48a1743bb5be1d30fd59638e4ea06fad
MoinMarcell <marcell.dechant@protonmail.com>
2023-01-27 00:11:57 +0100

    Update LoginRegisterDialog

--- frontend/src/components/dialogs/FliprLoginRegisterDialog.tsx
+++ frontend/src/components/dialogs/FliprLoginRegisterDialog.tsx
@@ -5,18 +5,23 @@ import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogTitle from '@mui/material/DialogTitle';
 import {Visibility, VisibilityOff} from "@mui/icons-material";
-import {FormControl, Input, InputAdornment, InputLabel} from "@mui/material";
+import {InputAdornment, TextField} from "@mui/material";
 import IconButton from "@mui/material/IconButton";
 import PersonIcon from '@mui/icons-material/Person';
-import PasswordIcon from '@mui/icons-material/Password';
-import {ChangeEvent, useCallback, useState} from "react";
+import {ChangeEvent, FormEvent, useCallback, useState} from "react";
 import {FliprUserDTO} from "../models/FliprUserDTO";
 import Box from "@mui/material/Box";
+import {wait} from "@testing-library/user-event/dist/utils";
+import PasswordIcon from '@mui/icons-material/Password';
+import FliprSnackBar from "../snackbar/FliprSnackBar";
+import {AlertColor} from "@mui/material/Alert";
+import {useNavigate} from "react-router";
 
 type FliprLoginDialogProps = {
     open: boolean,
     handleClose: () => void,
     login: (userToLogin: FliprUserDTO) => Promise<unknown>,
+    username: string,
 }
 
 export default function FliprLoginRegisterDialog(props: FliprLoginDialogProps) {
@@ -26,6 +31,25 @@ export default function FliprLoginRegisterDialog(props: FliprLoginDialogProps) {
         username: "",
         password: "",
     });
+    const [isError, setIsError] = useState<boolean>(false);
+    const [isDisabled, setIsDisabled] = useState<boolean>(false);
+    const [openSnackBar, setOpenSnackBar] = React.useState(false);
+    const [snackBarSeverity, setSnackBarSeverity] = useState<AlertColor | undefined>(undefined)
+    const [snackBarMessage, setSnackBarMessage] = useState<string>("");
+
+    const navigate = useNavigate();
+
+    const handleOpenSnackBar = useCallback(() => {
+        setOpenSnackBar(true);
+    }, []);
+
+    const handleCloseSnackbar = useCallback((event?: React.SyntheticEvent | Event, reason?: string) => {
+        if (reason === 'clickaway') {
+            return;
+        }
+
+        setOpenSnackBar(false);
+    }, []);
 
     const handleClickShowPassword = useCallback(() => {
         setShowPassword((show) => !show)
@@ -43,57 +67,96 @@ export default function FliprLoginRegisterDialog(props: FliprLoginDialogProps) {
         });
     }, [userCredentials]);
 
-    const handleLoginSubmit = useCallback(() => {
+    const handleLoginSubmit = useCallback((event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
         props.login(userCredentials)
             .then(() => {
-                props.handleClose();
+                setIsDisabled(true);
+                setIsError(false);
+                setSnackBarSeverity("success");
+                setSnackBarMessage("Welcome back, " + props.username + " :)");
+                handleOpenSnackBar();
+                wait(3000)
+                    .then(() => {
+                        props.handleClose();
+                        setUserCredentials({
+                            username: "",
+                            password: "",
+                        });
+                        navigate("/my-profile");
+                    });
             })
-            .catch()
-    }, [props, userCredentials]);
+            .catch((error) => {
+               if(error.response.status === 401){
+                   setIsDisabled(true);
+                   setIsError(true);
+                   setSnackBarSeverity("error");
+                   setSnackBarMessage("Username and/or Password are wrong.");
+                   handleOpenSnackBar();
+                   wait(3000)
+                       .then(() => {
+                           setIsError(false);
+                           setIsDisabled(false);
+                           setUserCredentials({
+                               username: "",
+                               password: "",
+                           });
+                       });
+               } 
+            });
+    }, [handleOpenSnackBar, navigate, props, userCredentials]);
 
     return (
-        <Dialog open={props.open} onClose={props.handleClose}>
+        <Dialog open={props.open} onClose={props.handleClose} keepMounted={props.open}>
             <DialogTitle>Login</DialogTitle>
-            <DialogContent>
-                <Box component={"form"} onSubmit={handleLoginSubmit}>
-                    <FormControl fullWidth sx={{m: 1}} variant="standard">
-                        <InputLabel htmlFor="standard-adornment-username">Username</InputLabel>
-                        <Input
-                            id="standard-adornment-username"
-                            value={userCredentials.username}
-                            name="username"
-                            onChange={handleChangeUserCredentials}
-                            startAdornment={<InputAdornment position="start"><PersonIcon/></InputAdornment>}
-                        />
-                    </FormControl>
-                    <FormControl fullWidth sx={{m: 1}} variant="standard">
-                        <InputLabel htmlFor="standard-adornment-password">Username</InputLabel>
-                        <Input
-                            id="standard-adornment-password"
-                            type={showPassword ? 'text' : 'password'}
-                            value={userCredentials.password}
-                            name="password"
-                            onChange={handleChangeUserCredentials}
-                            startAdornment={<InputAdornment position="start"><PasswordIcon/></InputAdornment>}
-                            endAdornment={
-                                <InputAdornment position="end">
-                                    <IconButton
-                                        aria-label="toggle password visibility"
-                                        onClick={handleClickShowPassword}
-                                        onMouseDown={handleMouseDownPassword}
-                                    >
-                                        {showPassword ? <VisibilityOff/> : <Visibility/>}
-                                    </IconButton>
-                                </InputAdornment>
-                            }
-                        />
-                    </FormControl>
-                </Box>
-            </DialogContent>
-            <DialogActions>
-                <Button onClick={props.handleClose}>Cancel</Button>
-                <Button onClick={handleLoginSubmit}>Login</Button>
-            </DialogActions>
+            <Box component={"form"} onSubmit={handleLoginSubmit}>
+                <DialogContent>
+                    <TextField
+                        error={isError}
+                        disabled={isDisabled}
+                        label="Username"
+                        id="outlined-start-adornment"
+                        value={userCredentials.username}
+                        onChange={handleChangeUserCredentials}
+                        name={"username"}
+                        fullWidth
+                        sx={{m: 1}}
+                        InputProps={{
+                            startAdornment: <InputAdornment position="start"><PersonIcon/></InputAdornment>,
+                        }}
+                    />
+                    <TextField
+                        error={isError}
+                        disabled={isDisabled}
+                        label="Password"
+                        id="outlined-adornment-password"
+                        type={showPassword ? 'text' : 'password'}
+                        value={userCredentials.password}
+                        onChange={handleChangeUserCredentials}
+                        name={"password"}
+                        fullWidth
+                        sx={{m: 1}}
+                        InputProps={{
+                            startAdornment: <InputAdornment position="start"><PasswordIcon/></InputAdornment>,
+                            endAdornment: <InputAdornment position="end">
+                                <IconButton
+                                    aria-label="toggle password visibility"
+                                    onClick={handleClickShowPassword}
+                                    onMouseDown={handleMouseDownPassword}
+                                    edge="end"
+                                >
+                                    {showPassword ? <VisibilityOff/> : <Visibility/>}
+                                </IconButton>
+                            </InputAdornment>
+                        }}
+                    />
+                </DialogContent>
+                <DialogActions>
+                    <Button type={"submit"}>Login</Button>
+                </DialogActions>
+                <FliprSnackBar open={openSnackBar} severity={snackBarSeverity} handleClose={handleCloseSnackbar}
+                               message={snackBarMessage}/>
+            </Box>
         </Dialog>
     );
 }
