44b51446e1c3e1933a5f8d6f3d49c8d07ef8011a
MoinMarcell <marcell.dechant@protonmail.com>
2023-01-27 11:02:03 +0100

    Update public profile

--- frontend/src/components/profile/PublicProfile.tsx
+++ frontend/src/components/profile/PublicProfile.tsx
@@ -1,5 +1,42 @@
 import {useParams} from "react-router";
 import useUser from "../hooks/useUser";
+import React, {useCallback} from "react";
+import {Box, Button, Grid, Tabs, Typography} from "@mui/material";
+import AccountCircle from "@mui/icons-material/AccountCircle";
+import Tab from "@mui/material/Tab";
+
+interface TabPanelProps {
+    children?: React.ReactNode;
+    index: number;
+    value: number;
+}
+
+function TabPanel(props: TabPanelProps) {
+    const { children, value, index, ...other } = props;
+
+    return (
+        <div
+            role="tabpanel"
+            hidden={value !== index}
+            id={`simple-tabpanel-${index}`}
+            aria-labelledby={`simple-tab-${index}`}
+            {...other}
+        >
+            {value === index && (
+                <Box sx={{ p: 3 }}>
+                    <Typography>{children}</Typography>
+                </Box>
+            )}
+        </div>
+    );
+}
+
+function a11yProps(index: number) {
+    return {
+        id: `simple-tab-${index}`,
+        'aria-controls': `simple-tabpanel-${index}`,
+    };
+}
 
 export default function PublicProfile() {
 
@@ -8,10 +45,41 @@ export default function PublicProfile() {
 
     const {user} = useUser(username);
 
-    return (
-        <div>
-            <h1>This is the Profile of @{user.username}</h1>
-            <p>Total Fliprs: {user.fliprs.length}</p>
-        </div>
+    const [value, setValue] = React.useState(0);
+
+    const handleChange = useCallback((event: React.SyntheticEvent, newValue: number) => {
+        setValue(newValue);
+    }, []);
+
+    return(
+        <Grid container spacing={1} flexDirection={"column"} sx={{mt: 4}}>
+            <Grid item alignSelf={"center"}>
+                <AccountCircle sx={{fontSize: 70}} />
+            </Grid>
+            <Grid item alignSelf={"center"}>
+                <Typography variant={"h4"}>@{user.username}</Typography>
+            </Grid>
+            <Grid item alignSelf={"center"}>
+                <Button variant={"outlined"}>Follow me</Button>
+            </Grid>
+            <Grid item alignSelf={"center"}>
+                <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+                    <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
+                        <Tab label="Fliprs" {...a11yProps(0)} />
+                        <Tab label="Liked Fliprs" {...a11yProps(1)} />
+                        <Tab label="Followers" {...a11yProps(2)} />
+                    </Tabs>
+                </Box>
+                <TabPanel value={value} index={0}>
+                    Item One
+                </TabPanel>
+                <TabPanel value={value} index={1}>
+                    Item Two
+                </TabPanel>
+                <TabPanel value={value} index={2}>
+                    Item Three
+                </TabPanel>
+            </Grid>
+        </Grid>
     );
 }
