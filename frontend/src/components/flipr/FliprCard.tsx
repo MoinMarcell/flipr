3833179d11f6366bb345dea9012e8138f0416a54
MoinMarcell <marcell.dechant@protonmail.com>
2023-01-27 20:58:54 +0100

    Create flipr card & gallery

---
+++ frontend/src/components/flipr/FliprCard.tsx
@@ -0,0 +1,149 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardContent from '@mui/material/CardContent';
+import CardActions from '@mui/material/CardActions';
+import Avatar from '@mui/material/Avatar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import {red} from '@mui/material/colors';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import ShareIcon from '@mui/icons-material/Share';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import {Flipr} from "../models/Flipr";
+import {Badge, Divider, Grid} from "@mui/material";
+import {Link} from "react-router-dom";
+import {useNavigate} from "react-router";
+import {useCallback, useEffect, useState} from "react";
+import FliprSnackBar from "../snackbar/FliprSnackBar";
+import {AlertColor} from "@mui/material/Alert";
+
+type FliprCardProps = {
+    flipr: Flipr,
+    username: string,
+    isAuthenticated: boolean,
+    addFliprToFavorites(username: string, fliprId: string): Promise<unknown>,
+    isLikedFlipr(username: string, fliprId: string): Promise<unknown>,
+}
+export default function FliprCard(props: FliprCardProps) {
+
+    const navigate = useNavigate();
+
+    const date = new Date(props.flipr.dateTime);
+    const day = date.getDate();
+    const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    const month = months[date.getMonth()];
+    const year = date.getFullYear();
+    const dateToShow = month + " " + day + ", " + year;
+    const [isFavorite, setIsFavorite] = useState<boolean>(false);
+    const [openSnackBar, setOpenSnackBar] = useState<boolean | undefined>(false);
+    const [snackBarMessage, setSnackBarMessage] = useState<string>("");
+    const [snackBarSeverity, setSnackBarSeverity] = useState<AlertColor | undefined>(undefined);
+
+    const handleCloseSnackBar = useCallback((event?: React.SyntheticEvent | Event, reason?: string) => {
+        if (reason === 'clickaway') {
+            setOpenSnackBar(false);
+        }
+
+        setOpenSnackBar(false);
+    }, []);
+
+    const handleAvatarClick = useCallback(() => {
+        navigate("/profiles/" + props.flipr.author);
+    }, [navigate, props.flipr.author]);
+
+    const handleAddToFavoritesClick = useCallback(() => {
+        if (props.isAuthenticated) {
+            props.addFliprToFavorites(props.username, props.flipr.id)
+                .then(() => {
+                    console.log("Flipr added to your Favorites!");
+                })
+                .catch((e) => {
+                    if (e.response.status === 401) {
+                        setSnackBarMessage("You are not logged in!");
+                        setSnackBarSeverity("error");
+                        setOpenSnackBar(true);
+                    } else if (e.response.status === 400) {
+                        setSnackBarMessage("Flipr is already favorite!");
+                        setSnackBarSeverity("error");
+                        setOpenSnackBar(true);
+                    }
+                });
+        }
+    }, [props]);
+
+    const handleShareClick = useCallback(() => {
+        navigator.clipboard.writeText(window.location + "/fliprs/" + props.flipr.id)
+            .then(() => {
+                setSnackBarMessage("Link copied successfully to your Clipboard!");
+                setSnackBarSeverity("success");
+                setOpenSnackBar(true);
+            })
+            .catch(() => {
+                setSnackBarMessage("Something went wrong! Try again later.");
+                setSnackBarSeverity("error");
+                setOpenSnackBar(true);
+            })
+    }, [props.flipr.id]);
+
+    useEffect(() => {
+        if (!isFavorite && props.isAuthenticated) {
+            props.isLikedFlipr(props.username, props.flipr.id)
+                .then(() => {
+                    setIsFavorite(true);
+                })
+                .catch((error) => {
+                    if (error.response.status === 400) {
+                        setIsFavorite(false);
+                    }
+                    if(error.response.status === 401){
+                        console.log("NOT LOGGED IN!!!")
+                    }
+                })
+        }
+    }, [isFavorite, props])
+
+    return (
+        <Grid item sx={{width: '95%'}}>
+            <Card>
+
+                <CardHeader
+                    avatar={
+                        <Avatar sx={{bgcolor: red[500], cursor: 'pointer'}} aria-label="recipe"
+                                onClick={handleAvatarClick}>
+                            {props.flipr.author.charAt(0)}
+                        </Avatar>
+                    }
+                    action={
+                        <IconButton aria-label="settings">
+                            <MoreVertIcon/>
+                        </IconButton>
+                    }
+                    title={<Link to={"/profiles/" + props.flipr.author}>{props.flipr.author}</Link>}
+                    subheader={dateToShow}
+                />
+
+                <CardContent>
+                    <Typography variant="body2" color="text.secondary">
+                        {props.flipr.content}
+                    </Typography>
+                </CardContent>
+
+                <Divider variant={"middle"}/>
+
+                <CardActions disableSpacing>
+                    <IconButton aria-label="add to favorites" onClick={handleAddToFavoritesClick}>
+                        <Badge badgeContent={props.flipr.likes} color="primary">
+                            <FavoriteIcon color={isFavorite ? "error" : undefined} />
+                        </Badge>
+                    </IconButton>
+                    <IconButton aria-label="share" onClick={handleShareClick}>
+                        <ShareIcon/>
+                    </IconButton>
+                </CardActions>
+
+            </Card>
+            <FliprSnackBar open={openSnackBar} severity={snackBarSeverity} handleClose={handleCloseSnackBar} message={snackBarMessage} />
+        </Grid>
+    );
+}
---
+++ frontend/src/components/flipr/FliprGallery.tsx
@@ -0,0 +1,22 @@
+import {Flipr} from "../models/Flipr";
+import FliprCard from "./FliprCard";
+import {Grid} from "@mui/material";
+
+type FliprGalleryProps = {
+    fliprs: Flipr[],
+    username: string,
+    isAuthenticated: boolean,
+    addFliprToFavorites(username: string, fliprId: string): Promise<unknown>,
+    isLikedFlipr(username: string, fliprId: string): Promise<unknown>,
+}
+export default function FliprGallery(props: FliprGalleryProps) {
+    const fliprCard = props.fliprs.map((flipr) => {
+        return <FliprCard isLikedFlipr={props.isLikedFlipr} addFliprToFavorites={props.addFliprToFavorites} username={props.username} isAuthenticated={props.isAuthenticated} flipr={flipr} key={flipr.id}/>
+    }).reverse();
+
+    return (
+        <Grid container spacing={2} justifyContent={"center"} sx={{mt: '1%'}}>
+            {fliprCard}
+        </Grid>
+    );
+}
