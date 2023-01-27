020a9e12eeb08511f1981dc00dac2a165c52821b
MoinMarcell <marcell.dechant@protonmail.com>
2023-01-27 20:57:20 +0100

    Update app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,16 +5,20 @@ import useUsers from "./components/hooks/useUsers";
 import UserProfile from "./components/profile/UserProfile";
 import PublicProfile from "./components/profile/PublicProfile";
 import ProtectedRoutes from "./ProtectedRoutes";
+import useFliprs from "./components/hooks/useFliprs";
+import FliprGallery from "./components/flipr/FliprGallery";
 
 export default function App(){
 
     const {username, isAuthenticated, login, logout} = useUsers();
+    const {fliprs, addFliprToFavorites, isLikedFlipr} = useFliprs();
 
     return (
         <BrowserRouter>
             <FliprTopBar logout={logout} login={login} username={username} isAuthenticated={isAuthenticated} />
             <Routes>
-                <Route path={"/:username"} element={<PublicProfile />} />
+                <Route path={"/"} element={<FliprGallery isLikedFlipr={isLikedFlipr} username={username} isAuthenticated={isAuthenticated} addFliprToFavorites={addFliprToFavorites} fliprs={fliprs} />} />
+                <Route path={"/profiles/:username"} element={<PublicProfile isLikedFlipr={isLikedFlipr} username={username} isAuthenticated={isAuthenticated} addFliprToFavorites={addFliprToFavorites} />} />
                 <Route element={<ProtectedRoutes login={login} username={username} />}>
                     <Route path={"/my-profile"} element={<UserProfile username={username} />} />
                 </Route>
