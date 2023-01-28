8380d5a0c45681889c5de5b197537c249394d17c
Marcell <84642104+MoinMarcell@users.noreply.github.com>
2023-01-28 21:33:38 +0100

    Revert "Update app"
    This reverts commit 16ba20bc41859a5041f21ae6a73f1024af7e7352.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,7 @@
 import * as React from "react";
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {BrowserRouter, Routes} from "react-router-dom";
 import FliprTopBar from "./components/appBar/FliprTopBar";
 import useUsers from "./components/hooks/useUsers";
-import UserProfile from "./components/profile/UserProfile";
-import PublicProfile from "./components/profile/PublicProfile";
-import ProtectedRoutes from "./ProtectedRoutes";
 
 export default function App(){
 
@@ -14,10 +11,7 @@ export default function App(){
         <BrowserRouter>
             <FliprTopBar logout={logout} login={login} username={username} isAuthenticated={isAuthenticated} />
             <Routes>
-                <Route path={"/:username"} element={<PublicProfile />} />
-                <Route element={<ProtectedRoutes login={login} username={username} />}>
-                    <Route path={"/my-profile"} element={<UserProfile username={username} />} />
-                </Route>
+
             </Routes>
         </BrowserRouter>
     );
