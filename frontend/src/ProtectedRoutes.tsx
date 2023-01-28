dfaae64f543fd25321cf40d9c0c0b6ffe970dcb4
Marcell <84642104+MoinMarcell@users.noreply.github.com>
2023-01-28 21:33:38 +0100

    Revert "Update ProtectedRoutes"
    This reverts commit 2fa803081490b7f288df7f2f02919619f3b0a128.

--- frontend/src/ProtectedRoutes.tsx
+++ frontend/src/ProtectedRoutes.tsx
@@ -1,9 +1,7 @@
 import {Navigate, Outlet} from "react-router-dom";
-import {FliprUserDTO} from "./components/models/FliprUserDTO";
 
 type ProtectedRoutesProps = {
     username: string,
-    login: (userToLogin: FliprUserDTO) => Promise<unknown>,
 }
 
 const ProtectedRoutes = (props: ProtectedRoutesProps) => {
@@ -11,11 +9,7 @@ const ProtectedRoutes = (props: ProtectedRoutesProps) => {
     const isAuthenticated = props.username !== 'anonymousUser' && props.username !== undefined && props.username !== null;
 
     return (
-        isAuthenticated
-            ?
-            <Outlet/>
-            :
-            <Navigate to={"/"} />
+        isAuthenticated ? <Outlet /> : <Navigate to={"/"} />
     );
 }
 
