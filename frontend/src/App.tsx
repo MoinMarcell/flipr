37ff6c84ae326a653648050aa9322d44d68fcb01
Marcell <84642104+MoinMarcell@users.noreply.github.com>
2023-01-28 21:33:38 +0100

    Revert "Update theme"
    This reverts commit 7717a74af6eacf457268a2a204f17584686ed6ab.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,16 +8,6 @@ import ProtectedRoutes from "./ProtectedRoutes";
 import useFliprs from "./components/hooks/useFliprs";
 import FliprGallery from "./components/flipr/FliprGallery";
 import FliprBottomBar from "./components/appBar/FliprBottomBar";
-import {createTheme, ThemeProvider} from "@mui/material";
-
-const darkTheme = createTheme({
-    palette: {
-        mode: 'dark',
-        primary: {
-            main: '#1976d2',
-        },
-    },
-});
 
 export default function App() {
 
@@ -26,29 +16,25 @@ export default function App() {
 
     return (
         <BrowserRouter>
-            <ThemeProvider theme={darkTheme}>
-                <FliprTopBar logout={logout} login={login} username={username} isAuthenticated={isAuthenticated}/>
-                <Routes>
-                    <Route path={"/"}
-                           element={<FliprGallery deleteFlipr={deleteFlipr} isLikedFlipr={isLikedFlipr}
-                                                  username={username}
-                                                  isAuthenticated={isAuthenticated}
-                                                  addFliprToFavorites={addFliprToFavorites} fliprs={fliprs}/>}/>
-                    <Route path={"/profiles/:username"}
-                           element={<PublicProfile deleteFlipr={deleteFlipr} isLikedFlipr={isLikedFlipr}
-                                                   username={username}
-                                                   isAuthenticated={isAuthenticated}
-                                                   addFliprToFavorites={addFliprToFavorites}/>}/>
-                    <Route element={<ProtectedRoutes login={login} username={username}/>}>
-                        <Route path={"/my-profile"} element={<UserProfile username={username}/>}/>
-                    </Route>
-                </Routes>
-                {
-                    isAuthenticated ?
-                        <FliprBottomBar/> :
-                        ''
-                }
-            </ThemeProvider>
+            <FliprTopBar logout={logout} login={login} username={username} isAuthenticated={isAuthenticated}/>
+            <Routes>
+                <Route path={"/"}
+                       element={<FliprGallery deleteFlipr={deleteFlipr} isLikedFlipr={isLikedFlipr} username={username}
+                                              isAuthenticated={isAuthenticated}
+                                              addFliprToFavorites={addFliprToFavorites} fliprs={fliprs}/>}/>
+                <Route path={"/profiles/:username"}
+                       element={<PublicProfile deleteFlipr={deleteFlipr} isLikedFlipr={isLikedFlipr} username={username}
+                                               isAuthenticated={isAuthenticated}
+                                               addFliprToFavorites={addFliprToFavorites}/>}/>
+                <Route element={<ProtectedRoutes login={login} username={username}/>}>
+                    <Route path={"/my-profile"} element={<UserProfile username={username}/>}/>
+                </Route>
+            </Routes>
+            {
+                isAuthenticated ?
+                    <FliprBottomBar/> :
+                    ''
+            }
         </BrowserRouter>
     );
 
