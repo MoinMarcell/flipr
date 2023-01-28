8954b3e31799c3bc65f19ad52114e2224588f4ef
Marcell <84642104+MoinMarcell@users.noreply.github.com>
2023-01-28 21:33:38 +0100

    Revert "Update FliprApp"
    This reverts commit 9d62d0058eb7fc5a0f30f4b211d5493e61623ec5.

--- frontend/src/components/flipr/FliprApp.tsx
+++ frontend/src/components/flipr/FliprApp.tsx
@@ -2,14 +2,13 @@ import {Flipr} from "../model/Flipr";
 import {Box} from "@mui/material";
 import FliprGallery from "./FliprGallery";
 import FliprPost from "./FliprPost";
-import {CommentDTO} from "../model/FliprDTO";
+import {FliprDTO} from "../model/FliprDTO";
 
 type FliprAppProps = {
     fliprs: Flipr[],
     username: string,
-    saveFlipr(fliprToSave: CommentDTO): Promise<Flipr>,
+    saveFlipr(fliprToSave: FliprDTO): Promise<Flipr>,
     deleteFlipr(fliprId: string): Promise<string>,
-    likeFlipr(fliprId: string): Promise<string>,
 }
 
 export default function FliprApp(props: FliprAppProps){
@@ -17,7 +16,7 @@ export default function FliprApp(props: FliprAppProps){
     return(
         <Box>
             <FliprPost saveFlipr={props.saveFlipr} author={props.username} />
-            <FliprGallery likeFlipr={props.likeFlipr} deleteFlipr={props.deleteFlipr} username={props.username} fliprs={props.fliprs} />
+            <FliprGallery deleteFlipr={props.deleteFlipr} username={props.username} fliprs={props.fliprs} />
         </Box>
     );
 
