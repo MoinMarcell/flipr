846a83ff603a1ac86e6c18872da2f39814a5a6c6
Marcell <84642104+MoinMarcell@users.noreply.github.com>
2023-01-28 21:33:38 +0100

    Revert "Update app"
    This reverts commit a973ae38f5887caf6e3efeaa046d23bdcaf8137b.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,9 @@
 import * as React from "react";
-import {BrowserRouter, Routes} from "react-router-dom";
-import FliprTopBar from "./components/appBar/FliprTopBar";
-import useUsers from "./components/hooks/useUsers";
 
 export default function App(){
 
-    const {username, isAuthenticated, login, logout} = useUsers();
-
     return (
-        <BrowserRouter>
-            <FliprTopBar logout={logout} login={login} username={username} isAuthenticated={isAuthenticated} />
-            <Routes>
-
-            </Routes>
-        </BrowserRouter>
+        <h1>Welcome to FLIPR!</h1>
     );
 
 }
