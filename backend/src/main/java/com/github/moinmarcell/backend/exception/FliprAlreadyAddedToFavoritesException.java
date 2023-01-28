a3892e621d7f93625fbd8dc4fd6b1b2eb42ba3bb
Marcell <84642104+MoinMarcell@users.noreply.github.com>
2023-01-28 21:33:38 +0100

    Revert "Create exception handler for favorite flipr"
    This reverts commit 5526ed6945cfd2c2caf7a37129d95ea3efd27749.

--- backend/src/main/java/com/github/moinmarcell/backend/exception/FliprAlreadyAddedToFavoritesException.java
+++
@@ -1,7 +0,0 @@
-package com.github.moinmarcell.backend.exception;
-
-public class FliprAlreadyAddedToFavoritesException extends RuntimeException{
-    public FliprAlreadyAddedToFavoritesException() {
-        super("The Flipr is already in your Favorites!");
-    }
-}
